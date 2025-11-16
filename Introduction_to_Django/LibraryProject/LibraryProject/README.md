ee3a79816d0348ab29857b7941681adf7a493ef3
Emediong Francis <emediongfrancis@gmail.com>
2025-11-16 19:01:34 +0100

    create Django Project

--- Introduction_to_Django/LibraryProject/LibraryProject/README.md
+++ Introduction_to_Django/LibraryProject/LibraryProject/README.md
@@ -1,1 +1,1 @@
-New Django Project.
+New Django project.
--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!tljqdh+*(j=#pf&zc-5+x14j)z+5-b$*(c#1v5^*_izbk7$i%'
+SECRET_KEY = 'django-insecure-d328+e($@55f=19$ow)7ks)qas%dik^zt!n!x8spn6rs&eb*^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
