c2028dc0e9b17178bef5395686bd1d87598689b7
Emediong Francis <emediongfrancis@gmail.com>
2025-11-19 20:42:38 +0100

    Add CustomUserAdmin for enhanced user management in admin interface
    - Implemented CustomUserAdmin class to manage CustomUser model in the Django admin.
    - Added fields for displaying user information, including profile photo, in the admin list view.
    - Configured fieldsets for user creation and editing, improving the admin experience for user management.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,49 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
+
+from .models import Book, CustomUser
+
+
+# Custom User Admin
+class CustomUserAdmin(BaseUserAdmin):
+    """Admin interface for CustomUser model."""
+    
+    list_display = ('username', 'email', 'date_of_birth', 'profile_photo_display', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    
+    def profile_photo_display(self, obj):
+        """Display profile photo thumbnail in admin list."""
+        if obj.profile_photo:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="border-radius: 50%;" />',
+                obj.profile_photo.url
+            )
+        return "No photo"
+    profile_photo_display.short_description = 'Profile Photo'
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 @admin.register(Book)
