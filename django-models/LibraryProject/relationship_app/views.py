7ed68d34896678608e9a46944f18e29ae4f9ad20
Emediong Francis <emediongfrancis@gmail.com>
2025-11-19 18:24:39 +0100

    Refactor imports in views.py to separate Book and Library models for improved clarity.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
 def book_list_view(request):
